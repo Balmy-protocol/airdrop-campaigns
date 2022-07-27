fbd1971188588ae169dd55225819e3e5e6e14427
Ged <0xged@protonmail.com>
2022-07-27 17:03:09 -0300

    refactor: update interface to newest idea

--- solidity/contracts/MultipleExpirableAirdrops.sol
+++ solidity/contracts/MultipleExpirableAirdrops.sol
@@ -3,7 +3,40 @@ pragma solidity >=0.8.7 <0.9.0;
 
 import './utils/Governable.sol';
 import '../interfaces/IMultipleExpirableAirdrops.sol';
+import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+import '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
 
 abstract contract MultipleExpirableAirdrops is IMultipleExpirableAirdrops, Governable {
-  constructor(address _governor) Governable(_governor) {}
+  using SafeERC20 for IERC20;
+
+  IERC20 public immutable claimableToken;
+  mapping(bytes32 => TrancheInformation) public tranches;
+  mapping(bytes32 => bool) public claimedTranches;
+
+  constructor(address _governor, IERC20 _claimableToken) Governable(_governor) {
+    if (address(_claimableToken) == address(0)) revert ZeroAddress();
+    claimableToken = _claimableToken;
+  }
+
+  function createTranche(
+    bytes32 _trancheMerkleRoot,
+    uint112 _claimable,
+    uint32 _deadline
+  ) external {}
+
+  function claimAndSendToClaimee(
+    bytes32 _trancheMerkleRoot,
+    address _claimee,
+    uint112 _amount,
+    bytes32[] calldata _merkleProof
+  ) external {}
+
+  function claimAndTransfer(
+    bytes32 _trancheMerkleRoot,
+    uint112 _amount,
+    address _recipient,
+    bytes32[] calldata _merkleProof
+  ) external {}
+
+  function closeTranche(bytes32 _trancheMerkleRoot, address _recipient) external returns (uint112 _unclaimed) {}
 }
