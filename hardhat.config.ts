a2eaa50b287537b938d94e8031bfc82606ef2b8f
Sam Bugs <sambugs@protonmail.com>
2024-02-16 16:26:30 -0300

    chore: try with lower version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,7 +8,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.24',
+        version: '0.8.20',
         settings: {
           optimizer: {
             enabled: true,
